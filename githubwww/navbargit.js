2c824f475d22ce19968324ad683978788709507d
Anushka <95957335+anushka3002@users.noreply.github.com>
2022-02-09 01:41:03 +0530

    Add files via upload

---
+++ githubwww/navbargit.js
@@ -0,0 +1,47 @@
+function navbar(){
+    return ` <div id="navbar">
+
+    <div id="logo">
+    
+        <a href="/">
+    
+            
+            <img src="https://cdn.icon-icons.com/icons2/2429/PNG/128/github_logo_icon_147285.png">
+    
+    
+        </a>
+    
+    
+    </div>
+    
+    
+    <div>
+    
+        <input type="text" id="search-input" placeholder="Search or jump to...">
+    
+    </div>
+    
+    <div id="options">
+    
+        <ul id="list">
+    
+    
+           <a> <li>Pull requests</li></a>
+           <a>  <li>Issues</li></a>
+           <a> <li>Marketplace</li></a>
+           <a href="explore.html">  <li>Explore</li></a>
+    
+        </ul>
+    
+    </div>
+    
+    <div id="profile">
+        
+        <img id="profile-img" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.rSOtD5qeYMjsBMS3gVc2lAHaFj%26pid%3DApi&f=1" alt="">
+    
+    </div>
+    
+    
+    </div>`
+}
+export default navbar
